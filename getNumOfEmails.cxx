83b49f9fac735c61c3095aa1192edde32932e427
pico866 <61026616+pico866@users.noreply.github.com>
2020-02-13 11:56:49 -0800

    Update getNumOfEmails.cxx

--- getNumOfEmails.cxx
+++ getNumOfEmails.cxx
@@ -7,23 +7,28 @@ string stripEmailAddr(string str) {
 	string ret = "";
 	for(char c: str) {
 		if(c=='.') continue;//skip dot char
-		if(c=='@' || c=='+') break; //ignore after @ or +
-		ret += c;//otherwise keep concatenating char into return string "ret"
+		if(c=='@' || c=='+') break; //ignore domain name after @ or +
+		ret += c;//otherwise concatenate char into return string
 	}
 	return ret;
 }
 
 int getNumOfUniqueEmails(vector<string>& emailaddrs) {
+
 	unordered_set<string> set;
 	for(string email: emailaddrs) {
-		if(!email.empty())
-			set.insert(stripEmailAddr(email));
+		if(!email.empty()) { //don't bother if entier email addr is empty (e.g " ".)
+			string ret = stripEmailAddr(email);
+			if(!ret.empty())//dont enter empty user name (e.g ".@gmail.com")
+				set.insert(ret);
+		}
 	}
 	return set.size();
+
 }
 
 int main() {
-  vector<string> inputStr = {"test.email@gmail.com", "test.email+spam@gmail.com","testemail@gmail.com"};
+  vector<string> inputStr = {"",".@gmail.com","test.email@gmail.com", "test.email+spam@gmail.com","testemail@gmail.com"};
   cout << getNumOfUniqueEmails(inputStr) << endl;
   return 0;
 }
